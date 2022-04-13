7b0c832150b3bbe1a5d243715e3cb103e63a222e
Nikita Bushuev <mrbushik1@gmail.com>
2022-04-13 12:30:14 +0300

    ready menu

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -2,36 +2,44 @@ const menu = () => {
     const menuBtn = document.querySelector(".menu-block-btn");
     const dropMenu = document.querySelector(".drop-menu");
     const row = document.querySelectorAll(".header-row");
-    const closeMenuBtn = document.querySelector(".menu-active_btn")
+    const closeMenuBtn = document.querySelector(".menu-active_btn");
+    const header = document.querySelector(".header");
+
 
     let count = 0;
-    console.log(menuBtn);
-    console.log(dropMenu);
-    console.log(row);
+
+    console.log(header);
+
     menuBtn.addEventListener("click", () => {
+
         row.forEach((item) => {
-            item.classList.toggle("d-none")
-        })
+            item.classList.toggle("d-none");
+        });
+
+        header.classList.toggle("active-menu");
+        dropMenu.classList.toggle("d-none");
         closeMenuBtn.classList.toggle("d-none");
+
         if (!closeMenuBtn.classList.contains("d-none")) {
 
-            appearanceBtn();
-            closeMenuBtn.style.opacity = 0;
-        } else {
+            appearanceBtn(closeMenuBtn);
 
+        } else {
+            closeMenuBtn.style.opacity = 0;
         }
 
 
 
     });
 
-    const appearanceBtn = () => {
+    const appearanceBtn = (elem) => {
 
         const appearanceBtnTimer = setInterval(() => {
             count += 0.05;
-            closeMenuBtn.style.opacity = count;
+            elem.style.opacity = count;
             if (count >= 1) {
                 clearInterval(appearanceBtnTimer);
+                count = 0;
             }
         }, 10);
 
