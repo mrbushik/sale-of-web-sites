1dd59fea2675b5954f3f66faccfe0b50da1eadc8
Nikita Bushuev <mrbushik1@gmail.com>
2022-04-15 13:03:36 +0300

    хоть что то отправляется

--- src/modules/pop-up.js
+++ src/modules/pop-up.js
@@ -3,32 +3,29 @@ const popUp = () => {
     const popUpMenu = document.querySelector('.pop-up_menu');
     const closePopUp = document.querySelector('.pop-up_closeBtn');
     const thanksMenu = document.querySelector('.thanks-window');
-    const inputPhone = document.querySelector('.ppop-up_phone-input');
+    const inputPhone = document.querySelector('.pop-up_phone');
     const form = document.querySelector('.mobile-phone_form');
     const submitBtn = document.querySelector('.pop-up_submit');
-    console.log(leaveRequestBtn);
 
-    console.log(submitBtn);
+
+
+
+
+
     leaveRequestBtn.addEventListener('click', () => {
         popUpMenu.classList.remove('d-none');
     });
+
+
     closePopUp.addEventListener('click', () => {
         popUpMenu.classList.add('d-none');
     });
-    const validate = (list) => {
-        let success = true;
-        list.forEach((input) => {
-            //   if (!input.classList.contains('success')) {
-            if (input.value.trim() === '') {
-                success = false;
-                //проверка на буквы в имени
-                //проверка на количество цифр в номере
-            } else if (inputPhone.value.length < 4 || inputPhone.value.length > 16) {
-                success = false;
-            }
-        });
-        return success;
-    };
+    console.log(inputPhone);
+    inputPhone.addEventListener('input', (e) => {
+        e.target.value = e.target.value.replace(/[^\d\+]/, '');
+
+    });
+
     const sendData = (data) => {
         return fetch(' https://jsonplaceholder.typicode.com/posts', {
             method: 'POST',
@@ -40,46 +37,95 @@ const popUp = () => {
     };
 
     const submitForm = () => {
-
-        //  console.log(nameInput.value)
         const formDate = new FormData(form);
         const formBody = {};
-        const formElements = form.querySelectorAll('input');
 
+        const formElements = form.querySelectorAll('input');
+        console.log(formElements);
         formDate.forEach((val, key) => {
             formBody[key] = val;
         });
+        sendData(formBody)
+            .then(data => {
 
-
-        if (validate(formElements)) {
-            sendData(formBody)
-                .then(data => {
-                    console.log(data);
-                    formElements.forEach(input => {
-                        input.value = '';
-                    });
-                })
-                .catch(error => {
-
+                formElements.forEach(input => {
+                    input.value = '';
                 });
-        } else {
+            })
 
-            alert('заполните все формы');
+            .catch(error => {
+                console.log('some err');
+            });
 
-        }
     };
-    try {
-        if (!form) {
-            throw new Error('добавьте элемент');
-        }
-        form.addEventListener('submit', (e) => {
-            e.preventDefault();
-            submitForm();
-            console.log('good');
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        submitForm();
+        console.log('submit');
+    })
+    // const validate = (list) => {
+    //     let success = true;
+    //     list.forEach((input) => {
+    //         //   if (!input.classList.contains('success')) {
+    //         if (inputPhone.value.length < 4 || inputPhone.value.length > 16) {
+    //             success = false;
+    //         }
+    //     });
 
-        });
-    } catch (error) {
-        console.log(error.message);
-    }
+    //     return success;
+    // };
+    // const sendData = (data) => {
+    //     return fetch(' https://jsonplaceholder.typicode.com/posts', {
+    //         method: 'POST',
+    //         body: JSON.stringify(data),
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         }
+    //     }).then(res => res.json());
+    // };
+
+
+    // const submitForm = () => {
+
+    //     //  console.log(nameInput.value)
+    //     const formDate = new FormData(form);
+    //     const formBody = {};
+    //     const formElements = form.querySelectorAll('input');
+    //     console.log(formElements);
+    //     formDate.forEach((val, key) => {
+    //         formBody[key] = val;
+    //     });
+
+
+    //     if (validate(formElements)) {
+    //         sendData(formBody)
+    //             .then(data => {
+    //                 console.log(data);
+    //                 formElements.forEach(input => {
+    //                     input.value = '';
+    //                 });
+    //             })
+    //             .catch(error => {
+    //                 console.log('error');
+    //             });
+    //     } else {
+
+    //         alert('заполните все формы');
+
+    //     }
+    // };
+    // try {
+    //     if (!form) {
+    //         throw new Error('добавьте элемент');
+    //     }
+    //     form.addEventListener('submit', (e) => {
+    //         e.preventDefault();
+    //         submitForm();
+    //         console.log('good');
+
+    //     });
+    // } catch (error) {
+    //     console.log(error.message);
+    // }
 };
 export default popUp;
