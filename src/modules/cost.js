2fb92ce655529488d11756d85cb2f62bb9a7f213
Nikita Bushuev <mrbushik1@gmail.com>
2022-04-16 15:15:02 +0300

    bonus submit

--- src/modules/cost.js
+++ src/modules/cost.js
@@ -1,41 +1,44 @@
 const cost = () => {
     const btn = document.querySelectorAll('.next-btn');
     const targetItem = document.querySelectorAll('.form-target_item');
-    const form = document.querySelectorAll('.form-question')
+    const form = document.querySelectorAll('.form-question');
+    const formWrapper = document.querySelector('.price-form');
+    const submit = document.querySelector('submit-next_rectang');
     let count = 0;
     let nextForm = 1;
-    console.log(form);
     targetItem.forEach((item, index) => {
         item.addEventListener('click', (e) => {
             item.classList.toggle('item-active');
-            console.log(nextForm + " это некст форм НОВАЯ");
+            targetItem[14].classList.remove('item-active');
 
         });
     });
     btn.forEach((item, index) => {
         item.addEventListener('click', (e) => {
-            console.log('btn click');
 
             const checkItem = () => {
                 let success = false;
                 const itemActive = document.querySelectorAll('.item-active');
                 itemActive.forEach((item, index) => {
+                    if (count === 3) {
+                        item.classList.remove('item-active');
+                        formWrapper.classList.add('d-none')
+                        success = true;
+                    }
                     if (index >= 0) {
-                        console.log(nextForm + " это некст форм");
-
                         item.classList.remove('item-active');
                         success = true;
                     }
 
+
                 });
                 return success;
 
-            }
+            };
 
             if (checkItem()) {
                 form[count].classList.add('d-none');
                 form[nextForm].classList.remove('d-none');
-                console.log('good');
 
                 count++;
                 nextForm++;
